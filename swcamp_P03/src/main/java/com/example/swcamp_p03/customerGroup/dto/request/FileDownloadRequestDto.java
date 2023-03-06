c96b199c9b105a86fc0bc0535e41da5110a21b03
jsjune <avraskio@gmail.com>
2023-03-07 02:23:47 +0900

    :white_check_mark: 고객 그룹 서비스 다운로드 수정 #16

--- swcamp_P03/src/main/java/com/example/swcamp_p03/customerGroup/dto/request/FileDownloadRequestDto.java
+++ swcamp_P03/src/main/java/com/example/swcamp_p03/customerGroup/dto/request/FileDownloadRequestDto.java
@@ -3,5 +3,6 @@ package com.example.swcamp_p03.customerGroup.dto.request;
 import lombok.Getter;
 @Getter
 public class FileDownloadRequestDto {
+    private String downloadReason;
     private String password;
 }
