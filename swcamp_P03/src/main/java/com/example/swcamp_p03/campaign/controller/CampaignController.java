a959767782eea615215c4feec16c93218ddcd936
jsjune <avraskio@gmail.com>
2023-03-08 16:58:23 +0900

    :white_check_mark: 목록조회 리팩토링 및 검색 조회 구현 #29

--- swcamp_P03/src/main/java/com/example/swcamp_p03/campaign/controller/CampaignController.java
+++ swcamp_P03/src/main/java/com/example/swcamp_p03/campaign/controller/CampaignController.java
@@ -4,14 +4,19 @@ import com.example.swcamp_p03.campaign.dto.response.TotalCampaignResponseDto;
 import com.example.swcamp_p03.campaign.service.CampaignReadService;
 import com.example.swcamp_p03.common.dto.ResponseDto;
 import com.example.swcamp_p03.config.UserDetailsImpl;
+import com.example.swcamp_p03.customerGroup.dto.SearchDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +27,23 @@ public class CampaignController {
     @GetMapping("/campaigns")
     public ResponseDto<TotalCampaignResponseDto> getTotalCampaign(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                   @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
-                                          Pageable pageable) {
+                                                                  Pageable pageable) {
         return campaignReadService.getTotalCampaign(userDetails.getUser(), pageable);
+    }
+
+    @GetMapping("/campaigns/search")
+    public ResponseDto<TotalCampaignResponseDto> getSearch(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                           @RequestParam("keyword") String keyword,
+                                                           @RequestParam(value = "startDate", required = false, defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+                                                           @RequestParam(value = "endDate", required = false, defaultValue = "2300-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
+                                                           Pageable pageable) {
+        SearchDto searchDto = SearchDto.builder()
+                .user(userDetails.getUser())
+                .search(keyword)
+                .startDate(startDate)
+                .endDate(endDate)
+                .pageable(pageable)
+                .build();
+        return campaignReadService.getSearch(searchDto);
     }
 }
