acc9d94e34511595c0787e1694f2439d9f3d43ed
jsjune <avraskio@gmail.com>
2023-03-09 04:47:06 +0900

    :white_check_mark: 카피 그룹 - 목록조회 정렬 리팩토링 및 검색 기능 추가 #35

--- swcamp_P03/src/main/java/com/example/swcamp_p03/copyGroup/controller/CopyGroupController.java
+++ swcamp_P03/src/main/java/com/example/swcamp_p03/copyGroup/controller/CopyGroupController.java
@@ -9,10 +9,18 @@ import com.example.swcamp_p03.copyGroup.dto.response.CopyGroupCreateResponseDto;
 import com.example.swcamp_p03.copyGroup.dto.response.CopyGroupListResponseDto;
 import com.example.swcamp_p03.copyGroup.dto.response.CopyReportResponseDto;
 import com.example.swcamp_p03.copyGroup.service.CopyGroupService;
+import com.example.swcamp_p03.customerGroup.dto.SearchDto;
+import com.example.swcamp_p03.customerGroup.dto.reponse.TotalGroupResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,13 +31,30 @@ public class CopyGroupController {
 
     @GetMapping("/copies")
     public ResponseDto<CopyGroupListResponseDto> getCopyGroupList(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                                                 @RequestParam(name = "page", defaultValue = "0") int page,
-                                                                 @RequestParam(name = "size", defaultValue = "4") int size){
-        return ResponseDto.success(copyGroupService.copyGroupList( userDetails.getUser(), page, size));
+                                                                  @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
+                                                                  Pageable pageable) {
+        return ResponseDto.success(copyGroupService.copyGroupList(userDetails.getUser(), pageable));
+    }
+
+    @GetMapping("/copies/search")
+    public ResponseDto<CopyGroupListResponseDto> getSearch(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                        @RequestParam("keyword") String keyword,
+                                                        @RequestParam(value = "startDate",required = false,  defaultValue = "1900-01-01")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+                                                        @RequestParam(value = "endDate",required = false,  defaultValue = "2300-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
+                                                        Pageable pageable
+    ) {
+        SearchDto searchDto = SearchDto.builder()
+                .user(userDetails.getUser())
+                .search(keyword)
+                .startDate(startDate)
+                .endDate(endDate)
+                .pageable(pageable)
+                .build();
+        return ResponseDto.success(copyGroupService.getSearch(searchDto));
     }
 
     @PostMapping("/copy")
-    public ResponseDto<CopyGroupCreateResponseDto> createCopyGroup(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CopyGroupDto copyGroupDto){
+    public ResponseDto<CopyGroupCreateResponseDto> createCopyGroup(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CopyGroupDto copyGroupDto) {
         Long copyGroupId = copyGroupService.createCopyGroup(userDetails.getUser(), copyGroupDto);
         return ResponseDto.success(new CopyGroupCreateResponseDto(copyGroupId, "success"));
     }
@@ -39,23 +64,23 @@ public class CopyGroupController {
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable Long copyGroupId,
             @RequestBody CopyGroupDto copyGroupDto) {
-        return ResponseDto.success(copyGroupService.updateCopyGroup(userDetails.getUser(),copyGroupDto , copyGroupId));
+        return ResponseDto.success(copyGroupService.updateCopyGroup(userDetails.getUser(), copyGroupDto, copyGroupId));
     }
 
     @GetMapping("/copy/{copyGroupId}")
-    public ResponseDto<CopyGroupDto> getCopyGroup(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long copyGroupId){
+    public ResponseDto<CopyGroupDto> getCopyGroup(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long copyGroupId) {
         return ResponseDto.success(copyGroupService.getCopyGroup(userDetails.getUser(), copyGroupId));
     }
 
     @PostMapping("/gptcopy")
-    public ResponseDto<CreateCopyResponseDto> createCopy(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CreateCopyRequestDto requestDto) throws Exception{
+    public ResponseDto<CreateCopyResponseDto> createCopy(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CreateCopyRequestDto requestDto) throws Exception {
         String question = "브랜드이름 " + requestDto.getBrandName() + "은 " + requestDto.getSector() + "업종이야."
                 + requestDto.getBrandName() + "의 제품인 " + requestDto.getProductName() + "에 대해 "
                 + requestDto.getKeyword() + "를 포함해서 " + requestDto.getType() + " 문자메시지 형식으로 "
                 + requestDto.getCopyLength() + "글자에 맞춰 써줘.";
 
         CreateCopyResponseDto responseDto = new CreateCopyResponseDto();
-        for(int i = 0; i< requestDto.getCreateCount(); i++){
+        for (int i = 0; i < requestDto.getCreateCount(); i++) {
             responseDto.addCopy(copyGroupService.getGptCopy(question));
         }
 
@@ -63,7 +88,7 @@ public class CopyGroupController {
     }
 
     @PostMapping("/copy/{copyGroupId}/like")
-    public ResponseDto<CopyGroupLikeDto> CopyGroupLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long copyGroupId, @RequestBody CopyGroupLikeDto requestDto){
+    public ResponseDto<CopyGroupLikeDto> CopyGroupLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long copyGroupId, @RequestBody CopyGroupLikeDto requestDto) {
         copyGroupService.CopyGroupLike(userDetails.getUser(), copyGroupId, requestDto);
         return ResponseDto.success(requestDto);
     }
---
+++ swcamp_P03/src/main/java/com/example/swcamp_p03/copyGroup/repository/CopyGroupRepositoryImpl.java
@@ -0,0 +1,108 @@
+package com.example.swcamp_p03.copyGroup.repository;
+
+import com.example.swcamp_p03.copyGroup.dto.CopyGroupListElementDto;
+import com.example.swcamp_p03.copyGroup.dto.response.CopyGroupListResponseDto;
+import com.example.swcamp_p03.customerGroup.dto.SearchDto;
+import com.example.swcamp_p03.user.entity.User;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static com.example.swcamp_p03.copyGroup.entity.QCopyGroup.copyGroup;
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@RequiredArgsConstructor
+public class CopyGroupRepositoryImpl implements CopyGroupRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public CopyGroupListResponseDto findTotalCopyGroup(User user, Pageable pageable) {
+        List<CopyGroupListElementDto> result = jpaQueryFactory.select(Projections.constructor(CopyGroupListElementDto.class,
+                        copyGroup.copyGroupId,
+                        copyGroup.createdAt,
+                        copyGroup.favorite,
+                        copyGroup.coupGroupName))
+                .from(copyGroup)
+                .where(userEq(user))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(copyGroup.favorite.desc())
+                .orderBy(groupSort(pageable))
+                .fetch();
+
+        Long count = jpaQueryFactory.select(
+                        copyGroup.count()
+                )
+                .from(copyGroup)
+                .where(userEq(user))
+                .fetchOne();
+        PageImpl<CopyGroupListElementDto> copyGroupList = new PageImpl<>(result, pageable, count);
+        return new CopyGroupListResponseDto(copyGroupList.getTotalPages(), copyGroupList.getContent());
+    }
+
+    @Override
+    public CopyGroupListResponseDto findSearch(SearchDto searchDto) {
+        List<CopyGroupListElementDto> result = jpaQueryFactory.select(Projections.constructor(CopyGroupListElementDto.class,
+                        copyGroup.copyGroupId,
+                        copyGroup.createdAt,
+                        copyGroup.favorite,
+                        copyGroup.coupGroupName))
+                .from(copyGroup)
+                .where(userEq(searchDto.getUser()),
+                        containKeyword(searchDto.getSearch()),
+                        periodDate(searchDto.getStartDate(), searchDto.getEndDate()))
+                .offset(searchDto.getPageable().getOffset())
+                .limit(searchDto.getPageable().getPageSize())
+                .orderBy(copyGroup.favorite.desc())
+                .orderBy(copyGroup.favorite.desc(),copyGroup.copyGroupId.desc())
+                .fetch();
+
+        Long count = jpaQueryFactory.select(
+                        copyGroup.count()
+                )
+                .from(copyGroup)
+                .where(userEq(searchDto.getUser()))
+                .fetchOne();
+        PageImpl<CopyGroupListElementDto> copyGroupList = new PageImpl<>(result, searchDto.getPageable(), count);
+        return new CopyGroupListResponseDto(copyGroupList.getTotalPages(), copyGroupList.getContent());
+
+    }
+
+    private OrderSpecifier<?> groupSort(Pageable pageable) {
+        if (!pageable.getSort().isEmpty()) {
+            for (Sort.Order order : pageable.getSort()) {
+                Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+                if (order.getProperty().equals("createdAt")) {
+                    return new OrderSpecifier<>(direction, copyGroup.createdAt);
+                }
+            }
+        }
+        return null;
+    }
+
+    private BooleanExpression userEq(User user) {
+        return isEmpty(user) ? null : copyGroup.user.eq(user);
+    }
+
+    private BooleanExpression containKeyword(String search) {
+        return isEmpty(search) ? null : copyGroup.coupGroupName.contains(search);
+    }
+
+    private BooleanExpression periodDate(LocalDate startDate, LocalDate endDate) {
+        BooleanExpression isGoeStartDate = copyGroup.createdAt.goe(LocalDateTime.of(startDate, LocalTime.MIN));
+        BooleanExpression isLoeEndDate = copyGroup.createdAt.loe(LocalDateTime.of(endDate, LocalTime.MAX).withNano(0));
+        return Expressions.allOf(isGoeStartDate, isLoeEndDate);
+    }
+}
