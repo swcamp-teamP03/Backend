ecb6fea8d5ca66de8fbed4471efe51e58e97ff11
jsjune <avraskio@gmail.com>
2023-03-09 18:09:31 +0900

    :white_check_mark: 상세페이지 발송상태 데이터 동기화되게 수정 #40

--- swcamp_P03/src/main/java/com/example/swcamp_p03/campaign/service/CampaignReadService.java
+++ swcamp_P03/src/main/java/com/example/swcamp_p03/campaign/service/CampaignReadService.java
@@ -6,6 +6,7 @@ import com.example.swcamp_p03.campaign.dto.MessageADto;
 import com.example.swcamp_p03.campaign.dto.MessageBDto;
 import com.example.swcamp_p03.campaign.dto.response.CampaignDetailDto;
 import com.example.swcamp_p03.campaign.dto.response.TotalCampaignResponseDto;
+import com.example.swcamp_p03.campaign.entity.Campaign;
 import com.example.swcamp_p03.campaign.repository.CampaignMessageRepository;
 import com.example.swcamp_p03.campaign.repository.CampaignRepository;
 import com.example.swcamp_p03.common.dto.ResponseDto;
@@ -16,12 +17,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CampaignReadService {
     private final CampaignRepository campaignRepository;
-    private final CampaignMessageRepository campaignMessageRepository;
+    private final CampaignService campaignService;
 
     public ResponseDto<TotalCampaignResponseDto> getTotalCampaign(User user, Pageable pageable) {
         TotalCampaignResponseDto totalCampaign = campaignRepository.findTotalCampaign(user, pageable);
@@ -33,7 +35,9 @@ public class CampaignReadService {
         return ResponseDto.success(search);
     }
 
-    public ResponseDto<CampaignDetailDto> getDetailCampaign(Long campaignId) {
+    public ResponseDto<CampaignDetailDto> getDetailCampaign(Long campaignId) throws Exception {
+        Campaign findCampaign = campaignRepository.findById(campaignId).get();
+        campaignService.updateSendMessages(findCampaign);
         CampaignDetailDto campaign = campaignRepository.findByCampaign(campaignId);
         List<CustomerPropertyDto> properties = campaignRepository.findByCustomerGroupToProperties(campaignId);
         List<CopyWriteABDto> copyWriteAB = campaignRepository.findByCopyGroupToCopyWrites(campaignId);
